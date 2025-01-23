6d426ee970bf575d102818c89da6e6fc3efcb82d
Nandkishor1771503 <160572418+Nandkishor1771503@users.noreply.github.com>
2025-01-23 22:32:15 +0530

    hero sec mobile check .....

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -86,15 +86,15 @@ function Hero() {
 
   return (
     <>
-      <div className="hero lg:h-screen h-[100vh] w-full mx-[3%] md:mx-[20%] lg:mx-0">
+      <div className="hero lg:h-screen h-[100vh] w-full ml-[3%] md:ml-[20%] lg:mx-0">
         <div className="topLine w-7 bg-gray-50 rounded-full h-2 mb-[10%]">
           .
         </div>
-        <div className=" text-orange-500 flex font-thin lg:mx-[29%] lg:mt-[9%] md:mt-[5%] mx-5 mt-[50%] ">
+        <div className=" text-orange-500 flex font-thin lg:ml-[29%] lg:mt-[9%] md:mt-[5%] ml-2 mt-[45%]">
           {Title.map((text, id) => {
             return (
               <h1
-                className="title opacity-70 lg:text-8xl md:text-7xl text-6xl"
+                className="title opacity-70 lg:text-8xl md:text-7xl text-5xl"
                 key={id}
               >
                 {text}
@@ -103,10 +103,10 @@ function Hero() {
           })}
         </div>
 
-        <div className=" text-gray-400 flex gap-4 font-thin lg:mx-[38%] md:mx-[8%] lg:mt-[3%] mt-[10%] mx-[20%]">
+        <div className=" text-gray-400 flex gap-4 font-thin lg:ml-[38%] md:ml-[8%] lg:mt-[3%] mt-[10%] ml-[5%]">
           {discription.map((disp, i) => {
             return (
-              <h2 className=" disp md:text-3xl text-2xl" key={i}>
+              <h2 className="disp md:text-3xl text-2xl" key={i}>
                 {disp}
               </h2>
             );
@@ -115,7 +115,7 @@ function Hero() {
         <div className="bottomLine w-7 bg-white rounded-full h-2 mt-[8%]">
           .
         </div>
-        <div className="scroll lg:mx-[45%] md:ml-[20%] ml-[38%] lg:mt-7 md:mt-[8%] mt-[30%]  w-[60%] ">
+        <div className="scroll lg:mx-[45%] md:ml-[20%] ml-[20%] lg:mt-7 md:mt-[8%] mt-[27%]  w-[60%] ">
           <h2 className=" text-gray-500 text-3xl">Scroll to know</h2>
           <img src={Scroll} alt="scroll animation" className="ml-5" />
         </div>
