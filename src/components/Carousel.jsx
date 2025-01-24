33e7c0c9cd561bf6fcd10614701f059f55441535
Nandkishor1771503 <160572418+Nandkishor1771503@users.noreply.github.com>
2025-01-24 22:55:55 +0530

    carousel 2 ani pin

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -37,8 +37,8 @@ function Carousel() {
       ScrollTrigger.create({
         trigger: ".pin",
         pin: true, // Pin the entire ".pin" section
-        start: "top 300%",// Pin starts when ".pin" reaches the top of the viewport
-        end: "bottom 250%", // Adjust the scroll duration
+        start: "top 320%",// Pin starts when ".pin" reaches the top of the viewport
+        end: "bottom 350%", // Adjust the scroll duration
         scrub: 2,
         markers: true, // For debugging purposes
       });
@@ -47,10 +47,10 @@ function Carousel() {
       gsap.to(set, {
         xPercent: -125 * set.length, // Move all images horizontally
         scrollTrigger: {
-          trigger: ".container",
+          trigger: ".pin",
           // scroller:"html",
-          start: "top 300%", // Trigger starts when the container reaches the viewport
-          end: "bottom 250%", // Ensure this matches the ScrollTrigger duration
+          start: "top 320%", // Trigger starts when the container reaches the viewport
+          end: "bottom 330%", // Ensure this matches the ScrollTrigger duration
           scrub: 2,
           markers:true,
           // pin:true
