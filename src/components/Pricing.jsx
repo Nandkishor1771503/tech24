651ed0ff6d32a8a08086f229ea001f1bd44da365
Nandkishor1771503 <160572418+Nandkishor1771503@users.noreply.github.com>
2025-12-20 16:47:39 +0530

    Change pricing from INR to USD

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -48,7 +48,7 @@ function Pricing() {
           <h2 className=" text-orange-600 text-4xl  text-center">Basic</h2>
           <div className="list of services lg:ml-16 ml-5 flex flex-col gap-2">
             <span className="text-orange-500 lg:text-3xl text-2xl lg:mx-28 mx-24 lg:my-14 my-10 ">
-              ₹5,000
+              $199
             </span>
             {Basic.map((list, i) => {
               return (
@@ -73,7 +73,7 @@ function Pricing() {
 
           <div className="list of services lg:ml-16 ml-5 flex flex-col gap-0">
             <span className="text-orange-500 text-3xl lg:mx-28 mx-20 lg:my-14 my-10 ">
-              ₹10,000
+              $499
             </span>
             {Standard.map((list, i) => {
               return (
@@ -98,7 +98,7 @@ function Pricing() {
 
           <div className="list of services lg:ml-16 ml-5 flex flex-col gap-0">
             <span className="text-orange-500 text-3xl lg:mx-28 mx-20 lg:my-14 my-10 ">
-              ₹15,000
+              $799
             </span>
             {Premium.map((list, i) => {
               return (
